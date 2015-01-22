08c88025ef12d0b57991bb3f42fdee9546d96aaf
Lukas Heinrich <lukas.heinrich@gmail.com>
2015-01-22 16:07:57 +0100

    list dir

--- simple_file_blueprint.py
+++ simple_file_blueprint.py
@@ -3,6 +3,13 @@ from flask import Blueprint, send_from_directory, jsonify
 blueprint = Blueprint('fileserver', __name__,template_folder='templates')
 
 import os
+import subprocess
+from flask import Response
+
+@blueprint.route('/')
+def list():
+  p = subprocess.Popen(['ls','-l'],stdout = subprocess.PIPE)
+  return Response(p.stdout.readlines(), mimetype='text/plain')
 
 @blueprint.route('/<path:filepath>')
 def serve_file(filepath):
